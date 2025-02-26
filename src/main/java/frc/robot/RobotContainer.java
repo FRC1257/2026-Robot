681a404e8fd73c18a6ee436b460a963201d94d7a
ETsBrother <etsbrother1@gmail.com>
2025-02-26 09:35:22 -0500

    Refactoring changes (2/26)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -229,9 +229,9 @@ public class RobotContainer {
     COMBINED_STATION.onTrue(goToStation());
     COMBINED_STOW.onTrue(stow());
 
-    TOGGLE_REEF_POSITION_UP.onTrue(drive.positiveReefPoseToggle());
-    TOGGLE_REEF_POSITION_DOWN.onTrue(drive.negativeReefPoseToggle());
-    DRIVE_TO_REEF.onTrue(drive.DriveToReef());
+    TOGGLE_REEF_POSITION_UP.onTrue(drive.reefPoseChooserIncrement());
+    TOGGLE_REEF_POSITION_DOWN.onTrue(drive.reefPoseChooserDecrement());
+    DRIVE_TO_REEF.onTrue(drive.driveToReef());
 
     // operator.a().onTrue(coralPivot.quasistaticForward());
     // operator.b().onTrue(coralPivot.quasistaticBack());

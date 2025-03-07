b62fe50c3ace69015fba5c0b5e50d016909efc6c
bowenzarrow <148149696+bowenzarrow@users.noreply.github.com>
2025-03-07 18:00:51 -0500

    fixed paths (3/7)

--- src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
@@ -9,7 +9,6 @@ import static frc.robot.subsystems.vision.VisionConstants.numCameras;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.PhotonCamera;
@@ -83,7 +82,6 @@ public class VisionIOSim implements VisionIO {
 
     for (PhotonPoseEstimator estimator : camEstimators) {
       estimator.setReferencePose(currentEstimate);
-      estimator.addHeadingData(Timer.getFPGATimestamp(), currentEstimate.getRotation());
     }
 
     PhotonPipelineResult[] results = new PhotonPipelineResult[numCameras];

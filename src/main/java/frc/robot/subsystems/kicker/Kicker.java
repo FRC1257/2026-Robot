47f514ab88270906ae6253fbc1382a8fe11cba77
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2026-02-02 16:21:46 -0500

    Added 2 files

---
+++ src/main/java/frc/robot/subsystems/kicker/Kicker.java
@@ -0,0 +1,6 @@
+package frc.robot.subsystems.kicker;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Kicker extends SubsystemBase {
+}
---
+++ src/main/java/frc/robot/subsystems/kicker/KickerIO.java
@@ -0,0 +1,4 @@
+package frc.robot.subsystems.kicker;
+
+public interface KickerIO {
+}

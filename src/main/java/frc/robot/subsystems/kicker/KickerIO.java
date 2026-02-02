290fd70b556e14b5718fefe45142f99679581a90
AntiBlaze <175415025+AntiBlaze1@users.noreply.github.com>
2026-02-02 16:42:50 -0500

    Added Inputs

--- src/main/java/frc/robot/subsystems/kicker/KickerIO.java
+++ src/main/java/frc/robot/subsystems/kicker/KickerIO.java
@@ -5,7 +5,10 @@ import org.littletonrobotics.junction.AutoLog;
 public interface KickerIO {
     @AutoLog
     public static class KickerIOInputs {
-
+        public double velocity=0; //in RPM
+        public double appliedVoltage=0;
+        public double current=0; //amps
+        public double temperature=0; //celcius
     }
     public default void updateInputs(KickerIOInputs inputs) {}
 }

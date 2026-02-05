9b6c9ac289bdd2bec514a47d33e824e7f02d0a1f
greenbeen2906 <gchakravarty@ucvts.org>
2026-02-05 16:34:03 -0500

    Update FuelPivotIO.java

--- src/main/java/frc/robot/subsystems/fuelPivot/FuelPivotIO.java
+++ src/main/java/frc/robot/subsystems/fuelPivot/FuelPivotIO.java
@@ -1,17 +1,23 @@
 package frc.robot.subsystems.fuelPivot;
 
+import org.littletonrobotics.junction.AutoLog;a
+
 public interface FuelPivotIO {
     public static class FuelPivotIOInputs {
         //fill with inputs plz
     }
 
-    public default void undateInputs(FuelPivotIOInputs inputs) {}
+    public default void updateInputs(FuelPivotIOInputs inputs) {}
 
     public default void setVoltage (double motorVolts) {}
 
-    public default double getAngle() {}
+    public default double getAngle() {
+        return 0.0;
+    }
 
-    public default double getAngleVelocity() {}
+    public default double getAngleVelocity() {
+        return 0.0;
+    }
 
     public default void setSetpoint(double setpoint) {}
 
@@ -19,7 +25,9 @@ public interface FuelPivotIO {
 
     public default void setBrake() {}
 
-    public default boolean atSetpoint(){}
+    public default boolean atSetpoint(){
+        return false;
+    }
 
     public default void setP(double p) {}
 
@@ -35,17 +43,31 @@ public interface FuelPivotIO {
 
     public default void setkA(double kA) {}
 
-    public default double getP() {}
+    public default double getP() {
+        return 0.0;
+    }
     
-    public default double getI() {}
+    public default double getI() {
+        return 0.0;
+    }
 
-    public default double getD() {}
+    public default double getD() {
+        return 0.0;
+    }
 
-    public default double getkS() {}
+    public default double getkS() {
+        return 0.0;
+    }
 
-    public default double getkV() {}
+    public default double getkV() {
+        return 0.0;
+    }
 
-    public default double getkG() {}
+    public default double getkG() {
+        return 0.0;
+    }
 
-    public default double getkA () {}
+    public default double getkA () {
+        return 0.0;
+    }
 }

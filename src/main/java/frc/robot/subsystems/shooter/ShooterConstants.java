462bda39c3f58c23cfc364b182f36e8de1c7367d
EliasChang7 <eliaszychang@gmail.com>
2026-02-05 16:01:46 -0500

    constants

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.shooter;
+
+public class ShooterConstants {
+
+  // Motor IDs
+  public static final int MOTOR_ID = 0;
+
+  // Speed
+  public static final double MAX_VELOCITY = 8;
+  public static final double MAX_ACCERLERATION=16;
+}

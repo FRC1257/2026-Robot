0caf41442df417ad0aa347bd3916f7a001dbb505
Arman1291 <arman.aliyev.2009@gmail.com>
2026-02-05 18:51:38 -0500

    Update sparkmax

---
+++ src/main/java/frc/robot/subsystems/intake/HopperIntakeIOSparkMax.java
@@ -0,0 +1,63 @@
+package frc.robot.subsystems.intake;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
+@AutoLog
+public class HopperIntakeIOSparkMax implements HopperIntakeIO {
+    private SparkFlex motor;
+    private AbsoluteEncoder encoder;
+
+ 
+  private SparkFlexConfig config = new SparkFlexConfig();
+
+public HopperIntakeIOSparkMax() {
+
+    motor = new SparkFlex(HopperIntakeConstants.HOPPER_INTAKE_MOTOR_ID, MotorType.kBrushless);
+   
+config
+        .idleMode(IdleMode.kBrake)
+        .voltageCompensation(12)
+        .smartCurrentLimit(NEO_VORTEX_CURRENT_LIMIT) //why not working
+        .inverted(true);
+
+encoder = motor.getEncoder(); //cant convert from rel to abs
+
+motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+ 
+  @Override
+  public void updateInputs(HopperIntakeIOInputs inputs) {
+    inputs.appliedVoltage = motor.getAppliedOutput() * motor.getBusVoltage();
+    inputs.currentAmps = new double[] {motor.getOutputCurrent()};
+    inputs.tempCelcius = new double[] {motor.getMotorTemperature()};
+    inputs.velocityRadsPerSec = encoder.getVelocity();
+  }
+
+  
+  @Override
+  public void setVoltage(double voltage) {
+    motor.setVoltage(voltage);
+    Logger.recordOutput("HopperIntake/Desired Voltage", voltage);
+  }
+
+  
+  @Override
+  public void setBrake(boolean brake) {
+    config.idleMode(brake ? IdleMode.kBrake : IdleMode.kCoast);
+    motor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+  }
+
+
+}
+

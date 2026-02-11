b02125b459fa97419c43835e6e4ed4e2f08ebf42
FRCTeam1257 <programmingteam1257@outlook.com>
2026-02-11 16:11:30 -0500

    drive testing

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.drive;
 
+import static frc.robot.Constants.PI;
+
 import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
@@ -37,17 +39,17 @@ public final class DriveConstants {
 
   // Angular offsets of the modules relative to the chassis in radians
   // Subject to change depending on what robot side is the front
-  public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2 + 1.924964;
-  public static final double kFrontRightChassisAngularOffset = 2 * Math.PI - 6.141987682;
-  public static final double kBackLeftChassisAngularOffset = -Math.PI + 5.41928;
-  public static final double kBackRightChassisAngularOffset = -3 * Math.PI / 2 + 5.958595;
+  public static final double kFrontLeftChassisAngularOffset = Math.PI;
+  public static final double kFrontRightChassisAngularOffset = Math.PI;
+  public static final double kBackLeftChassisAngularOffset = 0;
+  public static final double kBackRightChassisAngularOffset = 0;
 
   // SPARK MAX CAN IDs
   // Subject to change depending on what robot side is the front
   public static final int kFrontLeftDrivingCanId = 7;
   public static final int kRearLeftDrivingCanId = 12;
   public static final int kFrontRightDrivingCanId = 4;
-  public static final int kRearRightDrivingCanId = 9;
+public static final int kRearRightDrivingCanId = 9;
 
   // drive motors are timing out for some reason in the logs
 
--- src/main/java/frc/robot/util/drive/DriveControls.java
+++ src/main/java/frc/robot/util/drive/DriveControls.java
@@ -1,4 +1,4 @@
-package frc.robot.util.drive;
+package frc.robot.util.drive; 
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;

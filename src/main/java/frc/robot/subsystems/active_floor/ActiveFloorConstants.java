ab6519ded6232a36074463852ed0eed9ccf3d856
RyanNg19 <ryanng201018@gmail.com>
2026-02-12 16:28:05 -0500

    constants

--- src/main/java/frc/robot/subsystems/active_floor/ActiveFloorConstants.java
+++ src/main/java/frc/robot/subsystems/active_floor/ActiveFloorConstants.java
@@ -3,5 +3,7 @@ package frc.robot.subsystems.active_floor;
 public class ActiveFloorConstants {
     //arbitrary numbers
     public static final double Floor_Voltage_On = 10.0;
+    public static final double Floor_Intake_Voltage = 10.0;
+    public static final double Floor_Outtake_Voltage = -10.0;
     public static final int Floor_Motor_ID = 1;
 }

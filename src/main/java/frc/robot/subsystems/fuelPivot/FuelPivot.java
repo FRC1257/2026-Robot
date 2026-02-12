93f11ada099098155b9fa49a99c4be436a7abd50
greenbeen2906 <gchakravarty@ucvts.org>
2026-02-12 15:59:27 -0500

    basic .java commands

--- src/main/java/frc/robot/subsystems/fuelPivot/FuelPivot.java
+++ src/main/java/frc/robot/subsystems/fuelPivot/FuelPivot.java
@@ -1,5 +1,24 @@
 package frc.robot.subsystems.fuelPivot;
 
-public class FuelPivot {
+public class FuelPivot extends subsystemBase{
+
+    private final fuelPivotIO io;
+
+
+    @overide
+    public void periodic(){
+        io.updateInputs(inputs);
+
+
+    }
+
+    public void setBrake(boolean brake) {
+        io.setBrake(brake);
+    }
+
+    public void stop(){
+        io.stop();
+    }
     
+
 }

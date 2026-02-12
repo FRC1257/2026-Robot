a743470ac6fafecbba5cd053c2c168d6c66d2972
oorekie-jpg <oorekie@ucvts.org>
2026-02-12 16:13:13 -0500

    Added Voltage Command

--- src/main/java/frc/robot/subsystems/kicker/Kicker.java
+++ src/main/java/frc/robot/subsystems/kicker/Kicker.java
@@ -3,9 +3,7 @@ package frc.robot.subsystems.kicker;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import java.util.function.DoubleSupplier;
-
 import org.littletonrobotics.junction.Logger;
 
 public class Kicker extends SubsystemBase {
@@ -15,22 +13,33 @@ public class Kicker extends SubsystemBase {
     public Kicker(KickerIO io) {
         this.io = io;
         SmartDashboard.putData(getName(), this);
+>>>>>>> 2177b5f2dc3c06486ba75b889a96cdf036bb6406
+    }
+
+    private void setVoltage(Voltage voltage) {
+        io.setVoltage(voltage)
     }
 
     @Override
     public void periodic() {
         io.updateInputs(inputs);
         Logger.processInputs("Kicker",inputs);
-
     }
 
-    public Command stop() {
-        return run(()->io.setVoltage(0.0));
+    private Command runOutake() {
+        return runVoltage(() -> )
+    }
+    public Command stopCommand() {
+        return runOnce(this::stop)
+    }
+
+    private void stop() {
+        io.stop()
     }
 
     public Command runKickerCommand(DoubleSupplier voltage) {
         return this.runEnd(
-            () -> io.setVoltage(voltage.getAsDouble()),
+            () -> io.setVoltage(voltage.getAsDouble(12.0)),
             () -> io.setVoltage(0.0)
 
             );

be67ff5adb621a05fb292bc11a6ed1f98f12ed04
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2026-02-17 21:13:19 -0500

    Unfinished Flywheel Code

--- src/main/java/frc/robot/subsystems/Shooter/Flywheel/FlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/Shooter/Flywheel/FlywheelConstants.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems.Shooter.Flywheel;
 
 public class FlywheelConstants {
+
+    public static final double FLYWHEEL_KP = 0.0; 
+
     public static final double FLYWHEEL_VOLTAGE_TOLERANCE = 0.1;
+    public static final int FLYWHEEL_MOTOR_ID = 1;
+    public static final int FLYWHEEL_FOLLOWER_MOTOR_ID = 1;
 }
--- src/main/java/frc/robot/subsystems/Shooter/Flywheel/FlywheelIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/Shooter/Flywheel/FlywheelIOSparkMax.java
@@ -1,5 +1,63 @@
 package frc.robot.subsystems.Shooter.Flywheel;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import frc.robot.Constants;
+
 public class FlywheelIOSparkMax implements FlywheelIO {
-    
+
+    private final SparkFlex motor; 
+    private final SparkFlex followerMotor; 
+
+    private final RelativeEncoder encoder; 
+    private final RelativeEncoder followerEncoder;
+
+    private final SparkFlexConfig flywheelConfig;
+    private final SparkFlexConfig followerConfig;
+
+    private final SparkClosedLoopController controller; 
+    private final SimpleMotorFeedforward feedforward;
+
+    public FlywheelIOSparkMax() {
+
+        motor = new SparkFlex(FlywheelConstants.FLYWHEEL_MOTOR_ID, SparkFlex.MotorType.kBrushless);
+        followerMotor = new SparkFlex(FlywheelConstants.FLYWHEEL_FOLLOWER_MOTOR_ID, SparkFlex.MotorType.kBrushless);
+
+        encoder = motor.getEncoder();
+        followerEncoder = followerMotor.getEncoder();
+
+
+        flywheelConfig = new SparkFlexConfig();
+
+        flywheelConfig
+            .smartCurrentLimit(Constants.NEO_VORTEX_CURRENT_LIMIT)
+            .idleMode(IdleMode.kCoast)
+            .voltageCompensation(12.0)
+            .inverted(false);
+        
+
+        // NEEDS TO BE DETERMINED
+        flywheelConfig
+            .encoder
+            .positionConversionFactor(1.0)
+            .velocityConversionFactor(1.0);
+
+        flywheelConfig
+            .closedLoop
+            .p(FlywheelConstants.FLYWHEEL_KP);
+
+        followerConfig = new SparkFlexConfig();
+
+        followerConfig.apply(flywheelConfig);
+        followerConfig.follow(motor);
+
+
+    }
+
 }

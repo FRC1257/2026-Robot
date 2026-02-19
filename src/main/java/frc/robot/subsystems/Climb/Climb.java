14efbf54a10bfadb2712332d5723d63d3c8f4e77
EliasChang7 <eliaszychang@gmail.com>
2026-02-19 15:45:30 -0500

    commands + arman's io

--- src/main/java/frc/robot/subsystems/Climb/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb/Climb.java
@@ -1,3 +1,43 @@
+import frc.robot.subsystems.Climb;
+
+import java.lang.System.Logger;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Climb extends SubsystemBase{
-    
+    private final ClimbIO climbIO;
+    private final ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+
+    public Climb(ClimbIO io) {
+        this.climbIO = io;
+    }
+
+    @Override
+    public void periodic(){
+        climbIO.updateInputs(inputs);
+        Logger.processInputs("Climb", inputs);
+    }
+
+    private void setVoltage(Voltage voltage) {
+        climbIO.setVoltage(voltage);
+    }
+
+    public Command runVoltage(Voltage voltage) {
+        return run(() -> setVoltage(voltage.get()));
+    }
+
+    public Command stopCommand(){
+        return run(() -> climbIO.stop()).withName("Climb/Stop");
+    }
+
+    public void setBreak(boolean brake) {
+        climbIO.setBreak(brake);
+    }
+
 }
--- src/main/java/frc/robot/subsystems/Climb/ClimbIO.java
+++ src/main/java/frc/robot/subsystems/Climb/ClimbIO.java
@@ -1,3 +1,32 @@
-public class ClimbIO {
-    
+//needs a set voltage and run voltage, in java the stop would set voltage to 0,
+package frc.robot.subsystems.Climb;
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+
+
+public interface ClimbIO{
+   @AutoLog
+
+public static class ClimbIOInputs {
+
+    public AngularVelocity velocityRadsPerSec; 
+    public Voltage appliedVoltage;
+
 }
+
+    
+public default void updateInputs(ClimbIOInputs inputs) {}
+
+public default void setVoltage(Voltage voltage) {}
+
+//public default void setBrake(boolean brake) {}
+
+public default void stop() {}
+
+
+
+
+}
+

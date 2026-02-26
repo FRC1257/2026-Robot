9851ca0600da8e303ec0cee1fffab5d721f2bf49
raghavmani0129 <raghavrhino2829@icloud.com>
2026-02-26 16:22:04 -0500

    active floor

--- src/main/java/frc/robot/subsystems/ActiveFloor/ActiveFloorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/ActiveFloor/ActiveFloorIOSparkMax.java
@@ -1,5 +1,69 @@
 package frc.robot.subsystems.ActiveFloor;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.Constants;
+import frc.robot.subsystems.Shooter.Flywheel.FlywheelConstants;
+import edu.wpi.first.units.Units;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class ActiveFloorIOSparkMax implements ActiveFloorIO {
+    private final SparkFlex motor;
+    private final RelativeEncoder encoder;
+    private final SparkFlexConfig config;
+
     
+    public ActiveFloorIOSparkMax() {
+        motor = new SparkFlex(ActiveFloorConstants.ACTIVE_FLOOR_MOTOR_ID, SparkFlex.MotorType.kBrushless);
+        encoder = motor.getEncoder();
+        config = new SparkFlexConfig();
+
+        config
+            .smartCurrentLimit(Constants.NEO_VORTEX_CURRENT_LIMIT)
+            .idleMode(IdleMode.kCoast)
+            .voltageCompensation(12.0)
+            .inverted(false);
+        
+
+        // NEEDS TO BE DETERMINED
+        config
+            .encoder
+            .positionConversionFactor(1.0)
+            .velocityConversionFactor(1.0);
+
+        config
+            .closedLoop
+            .p(FlywheelConstants.FLYWHEEL_KP);
+
+        motor.configure(config, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters); ;
+    }
+
+    @Override
+    public void updateInputs(ActiveFloorIOInputs inputs) {
+        inputs.activeFloorVoltage = Units.Volts.of(motor.getAppliedOutput() * motor.getBusVoltage());
+    }
+
+    @Override
+    public void setVoltage(Voltage voltage) {
+        motor.setVoltage(voltage);
+    }
+
+    @Override
+    public void stop() {
+        motor.stopMotor();
+    }
+
+    @Override
+    public void setBreakMode(boolean enabled) {
+        config.idleMode(enabled ? IdleMode.kBrake : IdleMode.kCoast);
+        motor.configure(
+            config, com.revrobotics.ResetMode.kNoResetSafeParameters, com.revrobotics.PersistMode.kNoPersistParameters);
+    }
+
 }

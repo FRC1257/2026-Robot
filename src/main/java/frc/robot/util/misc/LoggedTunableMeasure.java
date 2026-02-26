35eaa05df6d3b38e6a7944aa8a1942aa5a848a0a
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2026-02-26 09:43:16 -0500

    Added Javadocs

--- src/main/java/frc/robot/util/misc/LoggedTunableMeasure.java
+++ src/main/java/frc/robot/util/misc/LoggedTunableMeasure.java
@@ -23,15 +23,32 @@ public class LoggedTunableMeasure<U extends Unit> implements Supplier<Measure<U>
 
     private final Map<Integer, Double> lastHasChangedValues = new HashMap<>();
 
+    /**
+     * Create a new LoggedTunableMeasure
+     * @param dashboardKey Key on dashboard
+     * @param displayUnit Unit to display the measure in on the dashboard
+     */
+
     public LoggedTunableMeasure(String dashboardKey, U displayUnit) {
         this.key = TABLE_KEY + "/" + dashboardKey;
         this.displayUnit = displayUnit;
     }
 
+    /**
+     * Create a new LoggedTunableMeasure with the default value
+     * @param dashboardKey Key on dashboard
+     * @param defaultValue Default value, also used to determine the display unit
+     */
+
     public LoggedTunableMeasure(String dashboardKey, Measure<U> defaultValue) {
         this(dashboardKey, defaultValue.unit());
         initDefault(defaultValue);
     }
+
+    /**
+     * Set the default value of the measure.
+     * @param defaultValue The default value, also used to determine the display unit. The default value can only be set once.
+     */
 
     public void initDefault(Measure<U> defaultValue) {
         if (!hasDefault) {
@@ -43,9 +60,15 @@ public class LoggedTunableMeasure<U extends Unit> implements Supplier<Measure<U>
         }
     }
 
-    @SuppressWarnings("unchecked")
+    /**
+     * Get the value of the measure. If in tuning mode and the value is in the dashboard, get it from the dashboard, otherwise return the default value. If no default value has been set, return zero in the display unit.
+      * @return The value of the measure
+      * @throws ClassCastException if no default value has been set and the display unit cannot be cast to a measure of type U
+     */
+
+    @SuppressWarnings({ "unchecked", "unused" })
     @Override
-    public Measure<U> get() {
+    public Measure<U> get() throws ClassCastException {
         if(!hasDefault) {
             return (Measure<U>) displayUnit.zero();
         }
@@ -53,14 +76,30 @@ public class LoggedTunableMeasure<U extends Unit> implements Supplier<Measure<U>
         return (Measure<U>) displayUnit.of(value);
     }
 
+    /**
+     * Get the value of the measure in the specified unit as a double.
+     * @return the value of the measure in the specified unit as a double
+     */
+
     public double in(U unit) {
         return get().in(unit);
     }
+
+    /**
+     * Get the display unit of the measure.
+     * @return The display unit of the measure
+     */
 
     public U unit() {
         return displayUnit;
     }
 
+    /**
+     * Check if the value of the measure has changed since the last time this method was called with the same id.
+     * @param id An identifier for the value being checked 
+     * @return true if the value has changed, false otherwise
+     */
+    
     public boolean hasChanged(int id) {
 
         double currentValue = get().in(displayUnit);

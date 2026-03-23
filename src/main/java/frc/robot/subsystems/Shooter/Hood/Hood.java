479d930f71bfd725c8843d09004aa5a737e00c13
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2026-03-23 02:55:38 -0400

    Clamped hood angle for safety

--- src/main/java/frc/robot/subsystems/Shooter/Hood/Hood.java
+++ src/main/java/frc/robot/subsystems/Shooter/Hood/Hood.java
@@ -6,6 +6,8 @@ import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.Second;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
+import static frc.robot.subsystems.Shooter.Hood.HoodConstants.HOOD_MAX_ANGLE;
+import static frc.robot.subsystems.Shooter.Hood.HoodConstants.HOOD_MIN_ANGLE;
 
 import java.util.function.Supplier;
 
@@ -13,6 +15,7 @@ import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.Debouncer;
@@ -39,6 +42,7 @@ import frc.robot.NautilusMechanism3d;
 import frc.robot.Robot;
 import frc.robot.subsystems.Hopper.HopperPivot.HopperPivotConstants;
 import frc.robot.subsystems.Shooter.ShooterTrajectoryCalculator;
+import frc.robot.util.Units.UnitUtil;
 import frc.robot.util.misc.LoggedTunableNumber;
 
 public class Hood extends SubsystemBase {
@@ -46,7 +50,6 @@ public class Hood extends SubsystemBase {
     private static final LoggedTunableNumber Kp = new LoggedTunableNumber("Hood/Kp", HoodConstants.HOOD_KP);
     private static final LoggedTunableNumber Ki = new LoggedTunableNumber("Hood/Ki", HoodConstants.HOOD_KI);
     private static final LoggedTunableNumber Kd = new LoggedTunableNumber("Hood/Kd", HoodConstants.HOOD_KD);
-    private static final LoggedTunableNumber tolerance = new LoggedTunableNumber("Hood/Tolerance", HoodConstants.HOOD_ANGLE_TOLERANCE);
 
     private static final LoggedTunableNumber Ks = new LoggedTunableNumber("Hood/Ks", HoodConstants.HOOD_KS);
     private static final LoggedTunableNumber Kg = new LoggedTunableNumber("Hood/Kg", HoodConstants.HOOD_KG);
@@ -55,8 +58,6 @@ public class Hood extends SubsystemBase {
     private static final LoggedTunableNumber maxVel = new LoggedTunableNumber("Hood/MaxVelocity", HoodConstants.HOOD_CONSTRAINTS.maxVelocity);
     private static final LoggedTunableNumber maxAccel = new LoggedTunableNumber("Hood/MaxAcceleration", HoodConstants.HOOD_CONSTRAINTS.maxAcceleration);
 
-    private static final LoggedTunableNumber homingVolts = new LoggedTunableNumber("Hood/HomingVoltage", HoodConstants.HOMING_VOLTAGE.in(Volts));
-    private static final LoggedTunableNumber homingVelocityThreshold = new LoggedTunableNumber("Hood/HomingVelocityThreshold", HoodConstants.HOMING_VELOCITY_THRESHOLD);
 
     private static final LoggedTunableNumber hubAngle = new LoggedTunableNumber("Hood/hubAngle", 0.0);
 
@@ -73,13 +74,9 @@ public class Hood extends SubsystemBase {
     private TrapezoidProfile.State setpoint = null;
 
 
-    private SysIdRoutine sysId; 
-
-
     private Angle goalAngle = Radians.of(0.0);
     private Angle hoodOffset = Radians.of(0.0);
 
-    private boolean isZeroed = false;
 
     public Hood(HoodIO io) {
         this.io = io; 
@@ -138,17 +135,6 @@ public class Hood extends SubsystemBase {
     }
 
     /**
-     * Sets the current position of the hood as the zero angle. 
-     * This should be called when the hood is at its minimum angle, as defined in HoodConstants,
-     * to ensure that all future angle commands are accurate.
-     */
-
-    public void zero() {
-        hoodOffset = HoodConstants.HOOD_MIN_ANGLE.minus(inputs.hoodAngle);
-        isZeroed = true;
-    }
-
-    /**
      * Runs the hood to the specified angle using a PID controller and feedforward, with the angle supplied by a Supplier.
      * @param angle a Supplier that provides the desired angle to run the hood to, in radians
      * @return a Command that runs the hood to the specified angle when executed
@@ -156,8 +142,9 @@ public class Hood extends SubsystemBase {
 
     public Command runAngleCommand(Supplier<Angle> angle) {
         return run(() -> {
-            goalAngle = angle.get();
-            goal = new TrapezoidProfile.State(angle.get().in(Radians),0);
+            Angle clamped = UnitUtil.clamp(angle.get(), HOOD_MIN_ANGLE, HOOD_MAX_ANGLE);
+            goalAngle = clamped;
+            goal = new TrapezoidProfile.State(clamped.in(Radians),0);
             setpoint = profile.calculate(LoggedRobot.defaultPeriodSecs, setpoint, goal);
 
             Logger.recordOutput("Hood/setpoint", setpoint.position);
@@ -165,7 +152,8 @@ public class Hood extends SubsystemBase {
 
             io.runAngle(setpoint.position, setpoint.velocity);
         }).beforeStarting(() -> {
-            goalAngle = angle.get();
+            Angle clamped = UnitUtil.clamp(angle.get(), HOOD_MIN_ANGLE, HOOD_MAX_ANGLE);
+            goalAngle = clamped;
             setpoint = new TrapezoidProfile.State(inputs.hoodAngle.in(Radians), inputs.hoodVelocity.in(RadiansPerSecond));
         });
     }
@@ -187,10 +175,6 @@ public class Hood extends SubsystemBase {
         .withName("Shooter/Hood/VoltageCommand");
     }
 
-    public Command runStatic() {
-        return runVoltageCommand(() -> Volts.of(Ks.get()));
-    }
-
     /**
      * Runs the hood to the angle specified by the {@link ShooterTrajectoryCalculator} using a PID controller and feedforward.
      * @return a Command that runs the hood to the target angle when executed
@@ -198,60 +182,6 @@ public class Hood extends SubsystemBase {
 
     public Command runTargetedCommand(Supplier<Pose2d> robotPose) {
         return runAngleCommand(() -> ShooterTrajectoryCalculator.getInstance().getStaticParameters(robotPose).hoodAngle());
-    }
-
-    /**
-     * Runs the hood at a constant voltage until it is zeroed, as determined by the hood velocity being below a certain threshold for a certain amount of time.
-     * @return a Command that runs the hood at a constant voltage until it is zeroed
-     */
-
-    public Command zeroCommand() {
-        return runVoltageCommand(() -> Volts.of(homingVolts.get()))
-            .raceWith(Commands.waitSeconds(0.5)
-                .andThen(Commands.waitUntil(() -> Math.abs(inputs.hoodVelocity.magnitude()) <= homingVelocityThreshold.get())))
-            .andThen(this::zero)
-            .withTimeout(3.0)
-        .withName("Shooter/Hood/ZeroCommand");
-    }
-
-    /**
-     * Runs the hood at a constant voltage in the forward direction until it reaches the maximum angle, as defined in HoodConstants.
-     * @return a Command that runs the hood at a constant voltage in the forward direction until it reaches the maximum angle
-     */
-
-    public Command quasistaticForward() { 
-        return sysId.quasistatic(Direction.kForward)
-            .until(() -> inputs.hoodAngle.in(Radians) >= HoodConstants.HOOD_MAX_ANGLE.in(Radians));
-    }
-
-    /**
-     * Runs the hood at a constant voltage in the reverse direction until it reaches the minimum angle, as defined in HoodConstants.
-     * @return a Command that runs the hood at a constant voltage in the reverse direction until it reaches the minimum angle
-     */
-
-    public Command quasistaticReverse() { 
-        return sysId.quasistatic(Direction.kReverse)
-            .until(() -> inputs.hoodAngle.in(Radians) <= HoodConstants.HOOD_MIN_ANGLE.in(Radians));
-    }
-
-    /**
-     * Runs the hood using the voltage output from the SysId routine in the forward direction until it reaches the maximum angle, as defined in HoodConstants. This will allow for more accurate system identification by accounting for changes in velocity and acceleration during the test.
-     * @return a Command that runs the hood using the voltage output from the SysId routine in the forward direction until it reaches the maximum angle
-     */
-
-    public Command dynamicForward() {
-        return sysId.dynamic(Direction.kForward)
-            .until(() -> inputs.hoodAngle.in(Radians) >= HoodConstants.HOOD_MAX_ANGLE.in(Radians));
-    }
-    
-    /**
-     * Runs the hood using the voltage output from the SysId routine in the reverse direction until it reaches the minimum angle, as defined in HoodConstants. This will allow for more accurate system identification by accounting for changes in velocity and acceleration during the test.
-     * @return a Command that runs the hood using the voltage output from the SysId routine in the reverse direction until it reaches the minimum angle
-     */
-    
-    public Command dynamicReverse() {
-        return sysId.dynamic(Direction.kReverse)
-            .until(() -> inputs.hoodAngle.in(Radians) <= HoodConstants.HOOD_MIN_ANGLE.in(Radians));
     }
 
 }
